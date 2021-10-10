d181d90d8cce43fc606d19c15f34df50eb6e1d1b
Felipe Rêgo <feliperrego@gmail.com>
2021-10-10 13:37:38 -0300

    Eslint config improvements + Pizza Flavor component changes

--- src/ui/components/PizzaFlavor/index.tsx
+++ src/ui/components/PizzaFlavor/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { PizzaFlavorContainer, PizzaList, PizzaOption, PizzaName, PizzaImage } from './styles';
+import {
+  PizzaFlavorContainer, PizzaList, PizzaOption, PizzaName, PizzaImage,
+} from './styles';
 
 export interface PizzaOption {
   id: string;
@@ -9,42 +11,40 @@ export interface PizzaOption {
 }
 
 interface PizzaFlavor {
-  pizzas: PizzaOption[]
+  options: PizzaOption[]
 }
 
-const PizzaFlavor: React.FC<PizzaFlavor> = ({pizzas}) => {
+const PizzaFlavor: React.FC<PizzaFlavor> = ({ options }) => {
   const [inactive, setInactive] = useState<string[]>([]);
 
   const isInverted = (key: number): boolean => {
-    const length = pizzas.length;
+    const { length } = options;
     const middle = Math.ceil(length / 2);
     return key >= middle;
-  }
+  };
 
   const activate = (id: string) => {
-    const inactivePizzas = pizzas.filter(pizza => pizza.id !== id);
-    setInactive(inactivePizzas.map(pizza => pizza.id));
-    console.log(inactive)
-  }
+    const inactivePizzas = options.filter((pizza) => pizza.id !== id);
+    setInactive(inactivePizzas.map((pizza) => pizza.id));
+  };
 
   const isInactive = (id: string): boolean => inactive.includes(id);
 
   return (
     <PizzaFlavorContainer>
       <PizzaList>
-        {pizzas?.map((pizza, key) => (
-            <PizzaOption
-              key={pizza.id}
-              invert={isInverted(key)}
-              onMouseEnter={() => activate(pizza.id)}
-              onMouseLeave={() => setInactive([])}
-              inactive={isInactive(pizza.id)}
-            >
-              <PizzaName>{pizza.name}</PizzaName>
-              <PizzaImage src={pizza.img}/>
-            </PizzaOption>
-          )
-        )}
+        {options?.map((pizza, key) => (
+          <PizzaOption
+            key={pizza.id}
+            invert={isInverted(key)}
+            onMouseEnter={() => activate(pizza.id)}
+            onMouseLeave={() => setInactive([])}
+            inactive={isInactive(pizza.id)}
+          >
+            <PizzaName>{pizza.name}</PizzaName>
+            <PizzaImage src={pizza.img} />
+          </PizzaOption>
+        ))}
       </PizzaList>
     </PizzaFlavorContainer>
   );
